9fef5fe7e3d85f0ece6ba4ccb102436947a1113c
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2012-10-25 15:10:20 +0200

    Cleaned up source

--- fuzzyopen/fuzzyopen.py
+++ fuzzyopen/fuzzyopen.py
@@ -19,7 +19,7 @@ class HtmlItemDelegate(QItemDelegate):
         self.document = QTextDocument(self)
         self.document.setDocumentMargin(0)
         self.hl_color = QApplication.palette().highlight().color()
-        self.hl_color = ",".join( [`self.hl_color.red()`, `self.hl_color.green()`, `self.hl_color.blue()`] )
+        self.hl_color = ",".join([repr(self.hl_color.red()), repr(self.hl_color.green()), repr(self.hl_color.blue())])
 
     def drawDisplay(self, painter, option, rect, text):
         point = rect.topLeft()
@@ -39,43 +39,43 @@ class FuzzyOpen(QDialog):
         self.projectPaths = []
 
         QDialog.__init__(self, parent)
-        uic.loadUi(os.path.join( os.path.dirname(__file__), "fuzzyopen.ui" ), self)
+        uic.loadUi(os.path.join(os.path.dirname(__file__), "fuzzyopen.ui"), self)
         self.setModal(True)
-        self.listUrl.setItemDelegate( HtmlItemDelegate(self.listUrl) )
+        self.listUrl.setItemDelegate(HtmlItemDelegate(self.listUrl))
         self.hideProgress()
 
         self.iconLoader = KIconLoader()
-        self.btnSettings.setIcon( QIcon(self.iconLoader.loadIcon("configure", KIconLoader.Small)) )
-        self.btnRefresh.setIcon( QIcon(self.iconLoader.loadIcon("view-refresh", KIconLoader.Small)) )
+        self.btnSettings.setIcon(QIcon(self.iconLoader.loadIcon("configure", KIconLoader.Small)))
+        self.btnRefresh.setIcon(QIcon(self.iconLoader.loadIcon("view-refresh", KIconLoader.Small)))
 
         self.config = KConfig("katefuzzyopenrc")
         configPaths = self.config.group("ProjectPaths")
         for key in configPaths.keyList():
-            path = configPaths.readPathEntry(key,"")
+            path = configPaths.readPathEntry(key, "")
             if not path.endswith("/"):
                 path += "/"
-            self.projectPaths.append( path )
+            self.projectPaths.append(path)
 
         configFilters = self.config.group("Filters")
         self.lister = DirLister()
         self.lister.fileFound.connect(self.listerFileFound)
         self.lister.directoryChanged.connect(self.listerDirChanged)
         self.lister.completed.connect(self.listerCompleted)
-        self.lister.setIncludeFilters( configFilters.readEntry("include", "") )
-        self.lister.setExcludeFilters( configFilters.readEntry("exclude", "~$\n\.bak$\n/\.") )
+        self.lister.setIncludeFilters(configFilters.readEntry("include", ""))
+        self.lister.setExcludeFilters(configFilters.readEntry("exclude", "~$\n\.bak$\n/\."))
 
     def showEvent(self, event):
         katerect = kate.mainInterfaceWindow().window().rect()
         diarect = self.rect()
-        diarect.moveCenter( katerect.center() )
-        self.move( diarect.topLeft() )
+        diarect.moveCenter(katerect.center())
+        self.move(diarect.topLeft())
 
         self.reset()
         self.list()
 
     def getProjectUrl(self, url):
         for path in self.projectPaths:
-            if url.url().startswith( path ):
+            if url.url().startswith(path):
                 return path
         return False
 
@@ -107,7 +107,7 @@ class FuzzyOpen(QDialog):
             self.reason = "Same path of %s" % url.fileName()
             self.rootPath = url.upUrl()
 
-        self.lister.list( self.rootPath, recurse = self.project != False )
+        self.lister.list(self.rootPath, recurse=self.project != False)
 
     def addFileUrl(self, url, reason=None):
         if url not in self.urls:
@@ -117,32 +117,32 @@ class FuzzyOpen(QDialog):
 
             item = QListWidgetItem()
             if self.project and path.startswith(self.project):
-                path = path[ len(self.project): ]
+                path = path[len(self.project):]
                 item.setWhatsThis(path)
-                item.setText( "<b>%s</b>: <i>%s</i>" % ( filename, path ) )
+                item.setText("<b>%s</b>: <i>%s</i>" % (filename, path))
             else:
                 item.setWhatsThis(filename)
-                item.setText( "<b>%s</b>" % filename )
+                item.setText("<b>%s</b>" % filename)
             if reason:
                 item.setToolTip(reason)
-            item.setIcon( QIcon(self.iconLoader.loadMimeTypeIcon(mime.iconName(), KIconLoader.Small)) )
+            item.setIcon(QIcon(self.iconLoader.loadMimeTypeIcon(mime.iconName(), KIconLoader.Small)))
             self.listUrl.addItem(item)
 
-            if url.fileName().find( self.txtFilter.text() ) < 0:
+            if url.fileName().find(self.txtFilter.text()) < 0:
                 self.listUrl.setItemHidden(item, True)
             self.urls.append(url)
 
             self.refreshFilter()
 
     def refreshFilter(self):
-        self.on_txtFilter_textEdited( self.txtFilter.text() )
+        self.on_txtFilter_textEdited(self.txtFilter.text())
 
     def on_txtFilter_textEdited(self, s):
         firstMatch = -1
-        pattern = re.compile( ".*".join( [re.escape(c) for c in s] ), re.I )
+        pattern = re.compile(".*".join([re.escape(c) for c in s]), re.I)
         for i in range(self.listUrl.count()):
             matched = pattern.search(self.listUrl.item(i).whatsThis())
-            if matched and firstMatch<0:
+            if matched and firstMatch < 0:
                 firstMatch = i
             self.listUrl.setItemHidden(self.listUrl.item(i), matched is None)
         self.listUrl.setItemSelected(self.listUrl.item(firstMatch), True)
@@ -169,10 +169,9 @@ class FuzzyOpen(QDialog):
                     self.listUrl.setItemSelected(self.listUrl.item(current_index), True)
                     current_index = -1
 
-
     def on_txtFilter_returnPressed(self):
-        if len(self.listUrl.selectedItems())>0:
-            self.on_listUrl_itemActivated( self.listUrl.selectedItems()[0] )
+        if len(self.listUrl.selectedItems()) > 0:
+            self.on_listUrl_itemActivated(self.listUrl.selectedItems()[0])
 
     def on_listUrl_itemActivated(self, item):
         self.lister.stop()
@@ -180,13 +179,13 @@ class FuzzyOpen(QDialog):
         i = self.listUrl.row(item)
         if 0 <= i < len(self.urls):
             url = self.urls[i]
-            kate.mainInterfaceWindow().activateView( kate.documentManager.openUrl(url) )
+            kate.mainInterfaceWindow().activateView(kate.documentManager.openUrl(url))
 
     @pyqtSignature("")
     def on_btnSettings_clicked(self):
         settingsDialog = SettingsDialog(kate.activeDocument().url(), self)
-        settingsDialog.txtIncludePatterns.setPlainText( "\n".join( [r.pattern for r in self.lister.includeFilters] ) )
-        settingsDialog.txtExcludePatterns.setPlainText( "\n".join( [r.pattern for r in self.lister.excludeFilters] ) )
+        settingsDialog.txtIncludePatterns.setPlainText("\n".join([r.pattern for r in self.lister.includeFilters]))
+        settingsDialog.txtExcludePatterns.setPlainText("\n".join([r.pattern for r in self.lister.excludeFilters]))
         for path in self.projectPaths:
             settingsDialog.listProjectPaths.addItem(path)
 
@@ -204,12 +203,12 @@ class FuzzyOpen(QDialog):
 
             configFilters = self.config.group("Filters")
             includeFilters = settingsDialog.txtIncludePatterns.toPlainText()
-            self.lister.setIncludeFilters( includeFilters )
-            configFilters.writeEntry( "include", includeFilters )
+            self.lister.setIncludeFilters(includeFilters)
+            configFilters.writeEntry("include", includeFilters)
 
             excludeFilters = settingsDialog.txtExcludePatterns.toPlainText()
-            self.lister.setExcludeFilters( excludeFilters )
-            configFilters.writeEntry( "exclude", excludeFilters )
+            self.lister.setExcludeFilters(excludeFilters)
+            configFilters.writeEntry("exclude", excludeFilters)
 
             self.config.sync()
 
@@ -258,7 +257,7 @@ class DirLister(QObject):
         for filter in filters.splitlines():
             if filter:
                 try:
-                    self.includeFilters.append( re.compile(filter) )
+                    self.includeFilters.append(re.compile(filter))
                 except re.error:
                     pass
 
@@ -267,7 +266,7 @@ class DirLister(QObject):
         for filter in filters.splitlines():
             if filter:
                 try:
-                    self.excludeFilters.append( re.compile(filter) )
+                    self.excludeFilters.append(re.compile(filter))
                 except re.error:
                     pass
 
@@ -309,7 +308,7 @@ class DirLister(QObject):
         if type(itemlist) != list:
             urlList = []
             for item in itemlist:
-                urlList.append( dict(url = item.url().url(), isDir = item.isDir()) )
+                urlList.append(dict(url=item.url().url(), isDir=item.isDir()))
             self.cache[self.cacheUrl] = urlList
 
         urls = []
@@ -325,7 +324,6 @@ class DirLister(QObject):
             path = url.url()
             if isDir and not path.endswith("/"):
                 path += "/"
-
 
             if self.includeFilters:
                 matched = False
@@ -346,22 +344,22 @@ class DirLister(QObject):
                 if matched:
                     continue
 
-
             if isDir:
-                if self.recursion<self.maxRecursion:
-                    self.dirStack.append((self.recursion+1, url))
+                if self.recursion < self.maxRecursion:
+                    self.dirStack.append((self.recursion + 1, url))
             else:
                 mime = KMimeType.findByUrl(url)[0]
                 if self.validMime(mime):
                     urls.append(url)
 
-        urls = sorted(urls, lambda a,b: -1 if len(a.url()) < len(b.url()) else 1)
+        urls = sorted(urls, lambda a, b: -1 if len(a.url()) < len(b.url()) else 1)
 
         for url in urls:
             self.fileFound.emit(url)
 
 
 dialog = None
+
 
 @kate.action('Fuzzy Open', shortcut="Meta+t", menu="File")
 def fuzzyOpen():
